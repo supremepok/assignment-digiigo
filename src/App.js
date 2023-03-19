5f08dacf4c2f745d6323094ef55733458e788117
supremepok <supremeofficial1002@gmail.com>
2023-03-19 09:01:04 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+function List() {
+  const [items, setItems] = useState([]);
+  const [text, setText] = useState('');
+
+  const handleChange = e => {
+    setText(e.target.value);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!text) return;
+    setItems([...items, { id: Date.now(), text }]);
+    setText('');
+  };
+
+  const handleDelete = id => {
+    const newItems = items.filter(item => item.id !== id);
+    setItems(newItems);
+  };
+
+  const handleEdit = (id, newText) => {
+    const newItems = [...items];
+    const index = newItems.findIndex(item => item.id === id);
+    newItems[index].text = newText;
+    setItems(newItems);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={text} onChange={handleChange} />
+        <button>Add</button>
+      </form>
+      <ul>
+        {items.map(item => (
+          <li key={item.id}>
+            {item.text}
+            <button onClick={() => handleDelete(item.id)}>Delete</button>
+            <button onClick={() => handleEdit(item.id, prompt('Enter new text', item.text))}>Edit</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
 
-export default App;
+export default List;
